7b8fd17a58cd6013b515842e3fade3fd15f607bb
Lucas Furtado <lucas_shiguioka@hotmail.com>
2022-04-11 15:42:01 +0000

    Initial state of frontend

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,17 +1,31 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import { Fragment } from 'react'
 import styled from 'styled-components'
 
 const NavBar = styled.div`
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen','Courier New', Courier, monospace;
     background-color: #333;
     color:#f1f1f1;
     height: 50px;
-    width: 100%;
+    width: 100vw;
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 20px;
-`
+`;
+
+const Button = styled.button`
+    background-color: #666;
+    color: #f1f1f1;
+    border: #f1f1f1 solid 1px;
+    padding: 10px;
+    font-size: 1.2rem;
+    font-weight: bold;
+    border-radius: 5px;
+    cursor: pointer;
+`;
+
 const Home = () => {
     return (
         <Fragment>
@@ -22,6 +36,7 @@ const Home = () => {
                         body {
                             background-color: #f1f1f1;
                             margin: 0;
+                            padding: 0;
                             font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
                         }
                     `}
@@ -29,7 +44,12 @@ const Home = () => {
                 </style>
             </Head>
             <NavBar>
-                <h1>Home</h1>
+                <div className="img-container">
+                    <Image src="/image/logo_anemia_poetry.png" alt="logo" width={'50px'} height={'50px'} />
+                </div>
+                <Button>
+                    Login
+                </Button>
             </NavBar>
         </Fragment>
     )
